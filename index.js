fe8cbe64af6f0741959d59494dc9b16779e38e27
Saurabh <saurabhmehta1601@gmail.com>
2022-12-29 20:58:15 +0530

    feat: setup multer for file uploads to disk storage

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
-import multer from "multer";
 import path from "path";
-import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
 
 // CONFIGURATIONS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
@@ -24,7 +20,29 @@ app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use(
+  "/assets",
+  express.static(path.join(process.cwd(), "public", "assets"))
+);
+
+// setup diskstorage for image uploads
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/assets");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`>>> express app running at ${PORT}`);
