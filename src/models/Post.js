03c54c97cbb51f872bb2d5f395e8d084cf0aad04
Saurabh <saurabhmehta1601@gmail.com>
2022-12-30 13:24:21 +0530

    feat: add mongoose schema for Post

---
+++ src/models/Post.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const PostSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    location: String,
+    description: String,
+    picturePath: String,
+    likes: {
+      type: Map, // MORE PERFORMANCE THAN ARRAY ( NOT HAVE TO SEARCH THROUGHT WHOLE ARRAY )
+      of: Boolean,
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", PostSchema);
+export default Post;
